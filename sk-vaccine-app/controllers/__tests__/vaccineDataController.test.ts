a2d6c531c8f59bd76d43110dcd64b31bd5525765
ConnerWithAnE <conner.hnatiuk@usask.ca>
2025-03-08 17:02:11 -0600

    Added 10 tests for vaccineController, added error types and fixed a few errors

--- sk-vaccine-app/controllers/__tests__/vaccineDataController.test.ts
+++ sk-vaccine-app/controllers/__tests__/vaccineDataController.test.ts
@@ -1,6 +1,7 @@
 import VaccineDataController from "@/controllers/vaccineDataController";
 import { VaccineDataService } from "@/services/vaccineDataService";
-import { VaccinePDFData }  from "@/interfaces/iVaccineData";
+import { VaccinePDFData } from "@/interfaces/iVaccineData";
+import { PDFDownloadError, VaccineListVersionError } from "@/utils/ErrorTypes";
 import logger from "@/utils/logger";
 
 // Mocking the logger
@@ -10,16 +11,32 @@ jest.mock("../../utils/logger", () => ({
   warn: jest.fn(),
 }));
 
+// This class allows for testing protected functions
+class TestableVaccineDataController extends VaccineDataController {
+  constructor(vaccineDataService: VaccineDataService) {
+    super(vaccineDataService);
+  }
+
+  public async vaccineListUpToDate() {
+    return super.vaccineListUpToDate();
+  }
+
+  public async updateVaccineList() {
+    return super.updateVaccineList();
+  }
+}
+
 // Mock the VaccineDataService class
 jest.mock("../../services/vaccineDataService");
 
-describe("VaccineDataController", () => {
+describe("VaccineDataController Tests", () => {
   let vaccineDataController: VaccineDataController;
   let mockVaccineDataService: jest.Mocked<VaccineDataService>;
 
   beforeEach(() => {
     // Create a mocked instance of VaccineDataService
-    mockVaccineDataService = new VaccineDataService() as jest.Mocked<VaccineDataService>;
+    mockVaccineDataService =
+      new VaccineDataService() as jest.Mocked<VaccineDataService>;
 
     // Explicitly mock methods of the service we expect to use
     mockVaccineDataService.vaccineListUpToDate = jest.fn();
@@ -48,55 +65,280 @@ describe("VaccineDataController", () => {
     jest.clearAllMocks();
   });
 
-  test("should return true if the vaccine list is updated", async () => {
-    // Mock the service methods used in the updateVaccines method
-    mockVaccineDataService.vaccineListUpToDate.mockResolvedValue(false);  // Mock vaccine list not up-to-date
-    mockVaccineDataService.updateVaccineList.mockResolvedValue(undefined);  // Mock successful update
-    mockVaccineDataService.compareExternalPDFs.mockResolvedValue([
-      {
-        productId: 1,
-        english: { formatId: 123, filename: "vaccine1_english.pdf" },
-        french: { formatId: 456, filename: "vaccine1_french.pdf" },
-      },
-    ] as VaccinePDFData[]); // Mock external PDFs
+  describe("updateVaccines() Tests", () => {
+    test("should update 1 vaccine successfully", async () => {
+      const expected = {
+        failed: 0,
+        success: true,
+        updated: 1,
+      };
 
-    // Mock the downloading and updating of the PDF
-    mockVaccineDataService.downloadVaccinePDF.mockResolvedValue("path/to/file.pdf");
-    mockVaccineDataService.updateLocalPDFFilenames.mockResolvedValue(undefined);
-    mockVaccineDataService.getLocalPDFFilenames.mockResolvedValue(["local_pdf_1.pdf", "local_pdf_2.pdf"]); // Mock the local PDF filenames
+      mockVaccineDataService.downloadVaccinePDF.mockImplementation(
+        async (productId: number, formatId: number) => {
+          if (productId === 321) {
+            throw new PDFDownloadError(productId);
+          } else {
+            return `https://publications.saskatchewan.ca/api/v1/products/${productId}/formats/${formatId}`; // Custom return for this case
+          }
+        }
+      );
 
-    const result = await vaccineDataController.updateVaccines();
+      // Mock the service methods used in the updateVaccines method
+      mockVaccineDataService.vaccineListUpToDate.mockResolvedValue(false); // Mock vaccine list not up-to-date
+      mockVaccineDataService.updateVaccineList.mockResolvedValue(undefined); // Mock successful update
+      mockVaccineDataService.compareExternalPDFs.mockResolvedValue([
+        {
+          productId: 11766,
+          english: { formatId: 123, filename: "vaccine1_english.pdf" },
+          french: { formatId: 456, filename: "vaccine1_french.pdf" },
+        },
+      ] as VaccinePDFData[]); // Mock external PDFs
 
-    expect(result).toBe(true);  // Expect the function to return true
-    expect(mockVaccineDataService.vaccineListUpToDate).toHaveBeenCalled();
-    expect(mockVaccineDataService.updateVaccineList).toHaveBeenCalled();
-    expect(mockVaccineDataService.compareExternalPDFs).toHaveBeenCalled();
-    expect(mockVaccineDataService.downloadVaccinePDF).toHaveBeenCalled();
-    expect(mockVaccineDataService.updateLocalPDFFilenames).toHaveBeenCalled();
-    expect(mockVaccineDataService.getLocalPDFFilenames).toHaveBeenCalled(); // Ensure getLocalPDFFilenames is called
+      const result = await vaccineDataController.updateVaccines();
+      console.log(result);
+
+      expect(result).toEqual(expected);
+    });
+
+    test("should update 10 vaccines successfully", async () => {
+      const expected = {
+        failed: 0,
+        success: true,
+        updated: 10,
+      };
+
+      mockVaccineDataService.downloadVaccinePDF.mockImplementation(
+        async (productId: number, formatId: number) => {
+          if (productId === 321) {
+            throw new PDFDownloadError(productId);
+          } else {
+            return `https://publications.saskatchewan.ca/api/v1/products/${productId}/formats/${formatId}`; // Custom return for this case
+          }
+        }
+      );
+
+      // Mock the service methods used in the updateVaccines method
+      mockVaccineDataService.vaccineListUpToDate.mockResolvedValue(false); // Mock vaccine list not up-to-date
+      mockVaccineDataService.updateVaccineList.mockResolvedValue(undefined); // Mock successful update
+      // Mock 10 products which will be fine to update
+      mockVaccineDataService.compareExternalPDFs.mockResolvedValue(
+        Array.from({ length: 10 }, (_, i) => ({
+          productId: 11766 + i, // Increment productId for variety
+          english: {
+            formatId: 123 + i,
+            filename: `vaccine${i + 1}_english.pdf`,
+          },
+          french: { formatId: 456 + i, filename: `vaccine${i + 1}_french.pdf` },
+        })) as VaccinePDFData[]
+      ); // Mock external PDFs
+
+      const result = await vaccineDataController.updateVaccines();
+      console.log(result);
+
+      expect(result).toEqual(expected);
+    });
+
+    test("should fail to update 10 vaccines gracefully", async () => {
+      const expected = {
+        failed: 10,
+        success: false,
+        updated: 0,
+      };
+
+      mockVaccineDataService.downloadVaccinePDF.mockImplementation(
+        async (productId: number, formatId: number) => {
+          throw new PDFDownloadError(productId);
+        }
+      );
+
+      // Mock the service methods used in the updateVaccines method
+      mockVaccineDataService.vaccineListUpToDate.mockResolvedValue(false); // Mock vaccine list not up-to-date
+      mockVaccineDataService.updateVaccineList.mockResolvedValue(undefined); // Mock successful update
+      // Mock 10 products which will be fine to update
+      mockVaccineDataService.compareExternalPDFs.mockResolvedValue(
+        Array.from({ length: 10 }, (_, i) => ({
+          productId: 11766 + i, // Increment productId for variety
+          english: {
+            formatId: 123 + i,
+            filename: `vaccine${i + 1}_english.pdf`,
+          },
+          french: { formatId: 456 + i, filename: `vaccine${i + 1}_french.pdf` },
+        })) as VaccinePDFData[]
+      ); // Mock external PDFs
+
+      const result = await vaccineDataController.updateVaccines();
+      console.log(result);
+
+      expect(result).toEqual(expected);
+    });
+
+    test("should fail to update 5 vaccines gracefully and succeed updating 5 vaccines", async () => {
+      const expected = {
+        failed: 5,
+        success: false,
+        updated: 5,
+      };
+
+      mockVaccineDataService.downloadVaccinePDF.mockImplementation(
+        async (productId: number, formatId: number) => {
+          if (productId % 2 == 0) {
+            throw new PDFDownloadError(productId);
+          } else {
+            return `https://publications.saskatchewan.ca/api/v1/products/${productId}/formats/${formatId}`;
+          }
+        }
+      );
+
+      // Mock the service methods used in the updateVaccines method
+      mockVaccineDataService.vaccineListUpToDate.mockResolvedValue(false); // Mock vaccine list not up-to-date
+      mockVaccineDataService.updateVaccineList.mockResolvedValue(undefined); // Mock successful update
+      // Mock 10 products which will be fine to update
+      mockVaccineDataService.compareExternalPDFs.mockResolvedValue(
+        Array.from({ length: 10 }, (_, i) => ({
+          productId: 11766 + i, // Increment productId for variety
+          english: {
+            formatId: 123 + i,
+            filename: `vaccine${i + 1}_english.pdf`,
+          },
+          french: { formatId: 456 + i, filename: `vaccine${i + 1}_french.pdf` },
+        })) as VaccinePDFData[]
+      ); // Mock external PDFs
+
+      const result = await vaccineDataController.updateVaccines();
+      console.log(result);
+
+      expect(result).toEqual(expected); // Expect the function to return true
+    });
+
+    test("Should fail to update 1 vaccine and fail gracefully", async () => {
+      const expected = {
+        success: false,
+        updated: 0,
+        failed: 1,
+      };
+
+      mockVaccineDataService.downloadVaccinePDF.mockImplementation(
+        async (productId: number, formatId: number) => {
+          if (productId === 321) {
+            throw new PDFDownloadError(productId);
+          } else {
+            return `https://publications.saskatchewan.ca/api/v1/products/${productId}/formats/${formatId}`; // Custom return for this case
+          }
+        }
+      );
+
+      mockVaccineDataService.vaccineListUpToDate.mockResolvedValue(false);
+      mockVaccineDataService.compareExternalPDFs.mockResolvedValue([
+        {
+          productId: 321,
+          english: { filename: "", formatId: 123 },
+          french: { filename: "", formatId: 456 },
+        },
+      ] as VaccinePDFData[]);
+
+      const result = await vaccineDataController.updateVaccines();
+      expect(result).toEqual(expected);
+    });
+
+    test("Should fail to update the second PDF but update the first one successfully", async () => {
+      const expected = {
+        success: false,
+        updated: 1,
+        failed: 1,
+      };
+
+      mockVaccineDataService.downloadVaccinePDF.mockImplementation(
+        async (productId: number, formatId: number) => {
+          if (productId === 321) {
+            throw new PDFDownloadError(productId);
+          } else {
+            return `https://publications.saskatchewan.ca/api/v1/products/${productId}/formats/${formatId}`; // Custom return for this case
+          }
+        }
+      );
+
+      mockVaccineDataService.vaccineListUpToDate.mockResolvedValue(false);
+      mockVaccineDataService.compareExternalPDFs.mockResolvedValue([
+        {
+          productId: 11766,
+          english: { filename: "", formatId: 123 },
+          french: { filename: "", formatId: 456 },
+        },
+        {
+          productId: 321,
+          english: { filename: "", formatId: 123 },
+          french: { filename: "", formatId: 456 },
+        },
+      ] as VaccinePDFData[]);
+
+      const result = await vaccineDataController.updateVaccines();
+      expect(result).toEqual(expected);
+    });
+
+    test("should handle errors in updateVaccines gracefully", async () => {
+      const expected = {
+        success: false,
+        updated: 0,
+        failed: 1,
+      };
+
+      mockVaccineDataService.vaccineListUpToDate.mockResolvedValue(false);
+      mockVaccineDataService.compareExternalPDFs.mockResolvedValue([
+        {
+          productId: 11766,
+          english: { filename: "", formatId: 123 },
+          french: { filename: "", formatId: 456 },
+        },
+      ] as VaccinePDFData[]);
+
+      mockVaccineDataService.downloadVaccinePDF.mockRejectedValue(
+        new PDFDownloadError(11766)
+      );
+
+      const result = await vaccineDataController.updateVaccines();
+
+      expect(result).toEqual(expected); // The function still returns true even in case of error
+    });
   });
 
-  test("should handle errors in updateVaccines gracefully", async () => {
-    mockVaccineDataService.vaccineListUpToDate.mockResolvedValue(false);
-    mockVaccineDataService.compareExternalPDFs.mockResolvedValue([
-      { productId: 1, english: { filename: "", formatId: 123 }, french: { filename: "", formatId: 456 } },
-    ] as VaccinePDFData[]);
+  describe("vaccineListUpToDate() Tests", () => {
+    let testableVaccineController: TestableVaccineDataController;
 
-    mockVaccineDataService.downloadVaccinePDF.mockRejectedValue(new Error("Download error"));
+    beforeEach(() => {
+      testableVaccineController = new TestableVaccineDataController(
+        mockVaccineDataService
+      );
+    });
 
-    const result = await vaccineDataController.updateVaccines();
+    afterEach(() => {
+      jest.clearAllMocks();
+    });
 
-    expect(result).toBe(true);  // The function still returns true even in case of error
-    expect(mockVaccineDataService.downloadVaccinePDF).toHaveBeenCalled();
-    expect(logger.error).toHaveBeenCalledWith("Error updating pdfs in updateVaccines");
-  });
+    test("should return false indicating vaccine list out of date", async () => {
+      mockVaccineDataService.getVaccineListVersionRemote.mockResolvedValue(2);
+      mockVaccineDataService.getVaccineListVersionLocal.mockResolvedValue(1);
 
-  test("should check if the vaccine list is up to date", async () => {
-    mockVaccineDataService.getVaccineListVersionRemote.mockResolvedValue(2);
-    mockVaccineDataService.getVaccineListVersionLocal.mockResolvedValue(1);
+      const result = await testableVaccineController.vaccineListUpToDate();
 
-    const result = await vaccineDataController["vaccineListUpToDate"]();  // Access private method if needed
+      expect(result).toBe(false); // Expect the function to return false
+    });
 
-    expect(result).toBe(false);  // Remote version is higher, list is not up to date
+    test("should return true indicating vaccine list is up to date", async () => {
+      mockVaccineDataService.getVaccineListVersionRemote.mockResolvedValue(1);
+      mockVaccineDataService.getVaccineListVersionLocal.mockResolvedValue(1);
+
+      const result = await testableVaccineController.vaccineListUpToDate();
+
+      expect(result).toBe(true); // Expect the function to return true
+    });
+
+    test("should throw error, the remote version number is less than local", async () => {
+      mockVaccineDataService.getVaccineListVersionRemote.mockResolvedValue(1);
+      mockVaccineDataService.getVaccineListVersionLocal.mockResolvedValue(2);
+
+      await expect(
+        testableVaccineController.vaccineListUpToDate()
+      ).rejects.toThrow(VaccineListVersionError); // Expect the funciton to throw an error
+    });
   });
 });
