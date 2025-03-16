6573c3370e9c02b4e3dc74a09d694c6b3a6fc065
ConnerWithAnE <conner.hnatiuk@usask.ca>
2025-03-16 17:45:19 -0600

    Adds a global COLOURS constant (I know colors is the standard way cause of other libraries but ALSO its only the US that uses that and so I will use colours lol)

---
+++ sk-vaccine-app/utils/colours.ts
@@ -0,0 +1,12 @@
+export const COLOURS = {
+  GREEN: "#0b6a41",
+  WHITE: "#ffffff",
+  BLACK: "#4D4E53",
+  YELLOW: "#f1c730",
+  LIGHT_GREY: "#D6D6D4",
+  GREY: "#9A9B9D",
+  DARK_GREY: "#4D4E53",
+
+  STATUS_RED: "FF0000",
+  SEARCHBAR_BG: "#EFE8EE",
+};
