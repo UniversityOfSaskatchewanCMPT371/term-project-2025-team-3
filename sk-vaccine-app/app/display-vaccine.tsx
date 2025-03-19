2f0cf280aa52f31fa8fd847a1f380d69243c6c6f
ConnerWithAnE <conner.hnatiuk@usask.ca>
2025-03-18 20:42:39 -0600

    Added the header and styled it

--- sk-vaccine-app/app/display-vaccine.tsx
+++ sk-vaccine-app/app/display-vaccine.tsx
@@ -1,9 +1,8 @@
 import assert from "assert";
 import { useLocalSearchParams } from "expo-router";
 import React from "react";
-import { ScrollView, View, Text, Dimensions } from "react-native";
+import { StyleSheet, View, Text, Dimensions } from "react-native";
 import Pdf from "react-native-pdf";
-import * as DocumentPicker from "expo-document-picker";
 import logger from "@/utils/logger";
 import { VaccineSheet } from "@/interfaces/iVaccineData";
 import * as FileSystem from "expo-file-system";
@@ -24,24 +23,53 @@ export default function DisplayVaccine() {
   };
   checkFileExists(pageData.pdfPath);
 
-  
-
   return (
     <>
-     <View style={{ flex: 1 }}>
- 
-    <View>
-      <Text>{pageData.vaccineName}</Text>
-      <Text>{pageData.starting}</Text>
-    </View>
-  
-  <Pdf
-    source={{ uri: pageData.pdfPath }}
-    trustAllCerts={false}
-    style={{ flex: 1, width: Dimensions.get("window").width }}
-  />
-</View>
+      <View style={{ flex: 1, backgroundColor: "#0B6A41" }}>
+        <View style={styles.headerCard}>
+          <Text style={styles.headerTitle}>{pageData.vaccineName}</Text>
+          <Text style={styles.headerSubtitle}>
+            Starting Age/Grade: {pageData.starting}
+          </Text>
+        </View>
 
+        <Pdf
+          source={{ uri: pageData.pdfPath }}
+          trustAllCerts={false}
+          style={{
+            flex: 1,
+            width: Dimensions.get("window").width - 12, // Subtracting for margin on both sides
+            marginLeft: 4, // Left margin
+            marginRight: 4, // Right margin
+            backgroundColor: "#0B6A41",
+          }}
+        />
+      </View>
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  headerCard: {
+    backgroundColor: "#FFF",
+    borderRadius: 8,
+    padding: 16,
+    margin: 8,
+    shadowColor: "#000000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 2,
+    elevation: 2,
+  },
+  headerTitle: {
+    fontSize: 35,
+    fontWeight: "semibold",
+    textDecorationLine: "underline",
+    marginBottom: 4,
+    color: "#000",
+  },
+  headerSubtitle: {
+    fontSize: 18,
+    color: "#000",
+  },
+});
