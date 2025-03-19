d7f06e5d3c4e5303ed2b4b3a5dd6ad4521882dfc
Marziyeh-bme <marziyehhassanisangani@gmail.com>
2025-03-18 19:55:09 -0600

    attach the setting button to header

--- sk-vaccine-app/app/_layout.tsx
+++ sk-vaccine-app/app/_layout.tsx
@@ -1,13 +1,14 @@
 import "reflect-metadata";
 import { Stack } from "expo-router";
 import { Image, View, Text } from "react-native";
-import { memo, useEffect } from "react";
+import { JSXElementConstructor, memo, useEffect } from "react";
 import React from "react";
 import SettingsButton from "@/components/settings-btn";
 import VaccineDataController from "@/controllers/vaccineDataController";
 import { VaccineDataService } from "@/services/vaccineDataService";
 import { useUpdateVaccineSheets } from "@/hooks/vaccineData";
 import logger from "@/utils/logger";
+import settingsButton from "@/components/settings-btn";
 
 const CustomHeader = memo(() => {
   const TITLE_TEXT = "Sask\nImmunize";
@@ -36,18 +37,33 @@ const CustomHeader = memo(() => {
   );
 });
 
+/**
+ * header of the application
+ * @param {VaccineDataController } vaccineController - must be properley defined
+ * @param  {VaccineDataService} VaccineDataService -must be properley defined
+ * @param {Function} useUpdateVaccineSheets -must return a valid value
+ * @returns {JSXElement} 
+ * -The header should contains both title and settingButton
+ * -updateResult should have been logged using logger.info without error
+ * -Stack components properly rendered without crashing
+ */
 export default function RootLayout() {
   const vaccineController = new VaccineDataController(new VaccineDataService());
   const updateResult = useUpdateVaccineSheets(vaccineController);
   logger.info(updateResult);
+  console.assert(updateResult != null && updateResult !=undefined, 'updated result cannot be null or undefined');
+  console.assert(typeof SettingsButton=='function' && typeof CustomHeader=='function', 'must be valid component');
   return (
     <Stack
       screenOptions={{
+        headerRight: () => <SettingsButton />,
         headerStyle: {
           backgroundColor: "#fff",
         },
         headerTitle: () => <CustomHeader />, // Memoize the header, supposed to prevent updates but I am unsure about that
+        
         headerTitleAlign: "center",
+        
       }}
     />
   );
