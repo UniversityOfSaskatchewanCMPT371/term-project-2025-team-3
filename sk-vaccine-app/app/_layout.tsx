37d7e9771b861b80c173655d75c69613ea8225b4
Marziyeh-bme <marziyehhassanisangani@gmail.com>
2025-03-20 21:33:53 -0600

    adding Logo

--- sk-vaccine-app/app/_layout.tsx
+++ sk-vaccine-app/app/_layout.tsx
@@ -1,6 +1,6 @@
 import "reflect-metadata";
 import { Stack } from "expo-router";
-import { Image, View, Text } from "react-native";
+import { Image, View, Text, StyleSheet } from "react-native";
 import { JSXElementConstructor, memo, useEffect } from "react";
 import React from "react";
 import SettingsButton from "@/components/settings-btn";
@@ -9,30 +9,28 @@ import { VaccineDataService } from "@/services/vaccineDataService";
 import { useUpdateVaccineSheets } from "@/hooks/vaccineData";
 import logger from "@/utils/logger";
 import settingsButton from "@/components/settings-btn";
+import { ScreenHeight } from "react-native-elements/dist/helpers";
 
 const CustomHeader = memo(() => {
-  const TITLE_TEXT = "Sask\nImmunize";
-  const saskLogo = require("@/assets/images/sasklogo-tra.png");
-
+  const saskLogo = require("@/assets/images/NursingLogo.webp");
+  /**
+   * Preconditions: Image file (NursingLogo.webp) must be exist in the directory.
+   * Postcondition: the header should contain the logo and the text properly
+   */
   return (
     <View
-      style={{
-        flexDirection: "row",
-        justifyContent: "center",
-        alignItems: "center",
-        marginTop: 5,
-      }}
+      style = {styles.headerContainer}
     >
       <Image
         source={saskLogo}
-        style={{ width: 70, height: 100, resizeMode: "contain" }}
+        style={styles.logo}
       />
-      <View style={{ alignItems: "flex-start" }}>
-        <Text style={{ fontSize: 25, lineHeight: 28 }}>College of Nursing</Text>
-        <Text style={{ fontSize: 38, lineHeight: 38, marginTop: -3 }}>
-          Immunize
+      <Text style = {styles.headerTitle}>Sask</Text>
+      {/* <View style={{alignItems: "flex-start" }}>
+        <Text style={{ fontSize: 15, lineHeight: 20, marginTop: 60, justifyContent: 'center', alignContent: 'center'}}>
+          Sask Vaccine Guide
         </Text>
-      </View>
+      </View> */}
     </View>
   );
 });
@@ -57,14 +55,47 @@ export default function RootLayout() {
     <Stack
       screenOptions={{
         headerRight: () => <SettingsButton />,
-        headerStyle: {
-          backgroundColor: "#fff",
-        },
+        headerStyle: styles.headerStyle,
         headerTitle: () => <CustomHeader />, // Memoize the header, supposed to prevent updates but I am unsure about that
-        
         headerTitleAlign: "center",
-        
       }}
     />
   );
 }
+
+
+const styles = StyleSheet.create({
+  headerContainer:{
+    position: 'absolute',
+    top:0,
+    right:0,
+    left:0,
+    backgroundColor: '#fff',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    paddingVertical: 5,
+    height: ScreenHeight/8,
+    width: '100%',
+
+  },
+  logo: {
+    width: 250,
+    height: 70,
+    resizeMode: 'contain',
+    margin: 5,
+  },
+  headerTitle: {
+    fontFamily: 'MYRIADPRO-REGULAR',
+    fontSize: 16,
+    fontWeight: "600",
+    color: "#333", 
+    marginTop: -5,
+    textAlign: 'center'
+    
+  },
+  headerStyle: {
+    backgroundColor: '#fff',
+    shadowColor: '#000',
+
+  },
+})
